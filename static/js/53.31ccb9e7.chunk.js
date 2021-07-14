3243c1f8e86ec265b3cc198f7e75ea78212bc85f
Meng-Yuan Huang <myh@live.com>
2021-07-14 19:30:12 +0800

    3.0.2

---
+++ static/js/53.31ccb9e7.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptwdi=this.webpackJsonptwdi||[]).push([[53],{455:function(t,n,e){"use strict";e.r(n),e.d(n,"startStatusTap",(function(){return c}));var i=e(23),o=e(21),c=function(){var t=window;t.addEventListener("statusTap",(function(){Object(i.h)((function(){var n=t.innerWidth,e=t.innerHeight,c=document.elementFromPoint(n/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(t){return Object(o.c)(r,t)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.31ccb9e7.chunk.js.map
--- static/js/53.6aaae88c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptwdi=this.webpackJsonptwdi||[]).push([[53],{458:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(21),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.6aaae88c.chunk.js.map
